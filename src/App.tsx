a67da264096d100d28dd5119181f6184f2b4fc5e
JayHoney <jhoney92021@gmail.com>
2022-01-08 17:14:23 -0700

    feat: add sticky the man

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Container,
+  Grid,
+  Header,
+  Label,
+  GridRow,
+  GridColumn
+} from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid>
+      <GridRow>
+        <GridColumn>
+        <Label image='src/stickyTheMan.jpg' />
+
+          <img src="src/stickyTheMan.jpg" alt="stickyTheMan"/>
+        </GridColumn>
+        <GridColumn>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </GridColumn>
+      </GridRow>
+    </Grid>
   );
 }
 
